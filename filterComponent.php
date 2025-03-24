f622974179a92eec7229345081d1a1be422bb77d
Kim Louise <kimlabrador71@gmail.com>
2025-03-24 22:17:44 +0800

    Update filterComponent to use POST method for parameters and adjust loadData function

--- filterComponent.php
+++ filterComponent.php
@@ -1,12 +1,12 @@
 <?php
-include "connect.php"; // Include the database connection
+include "connect.php";
 
-if (!isset($_GET['table'], $_GET['field'])) {
+if (!isset($_POST['table'], $_POST['field'])) {
     exit(json_encode(["error" => "Missing table or field parameters"]));
 }
 
-$table = $_GET['table'];
-$field = $_GET['field'];
+$table = $_POST['table'];
+$field = $_POST['field'];
 
 $query = "SELECT DISTINCT `$field` FROM `$table`";
 $result = mysqli_query($conn, $query) 
