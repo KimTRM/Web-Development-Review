87af621176a68d1feafa051ee79fe26fbd751370
Kim Louise <kimlabrador71@gmail.com>
2025-03-25 12:11:52 +0800

    Organized the Files into Different Folders

--- day1/filterComponent.php
+++ day1/filterComponent.php
@@ -9,8 +9,7 @@ $table = $_POST['table'];
 $field = $_POST['field'];
 
 $query = "SELECT DISTINCT `$field` FROM `$table`";
-$result = mysqli_query($conn, $query) 
-        or exit(json_encode(["error" => mysqli_error($conn)]));
+$result = mysqli_query($conn, $query) or exit(json_encode(["error" => mysqli_error($conn)]));
 
 $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
 echo json_encode($data);
